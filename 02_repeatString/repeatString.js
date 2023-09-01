c4a9539fa2a7c004941524242e925b57ed804bb2
Rina-Mbugua <134137276+Rina-Mbugua@users.noreply.github.com>
2023-09-01 15:45:27 +0300

    add solution to 02_repeatString

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,16 @@
 const repeatString = function() {
 
+    if (num < 0 || !Number.isInteger(num)) {
+        return 'ERROR';
+    }
+
+    let result = '';
+
+    for (let i = 0; i < num; i++) {
+        result += str; // concatenate the string 'num' times
+    }
+
+    return result;
 };
 
 // Do not edit below this line
