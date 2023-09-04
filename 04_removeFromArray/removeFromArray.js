6b6dfef57ff26d8a4fef498e4a1563cfdfe2fdbb
Rina-Mbugua <134137276+Rina-Mbugua@users.noreply.github.com>
2023-09-04 10:55:45 +0300

    Solution to satisfy the first test.

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,6 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(arr, valueToRemove) {
+    // Use the filter method to create a new array that excludes the value to remove.
+    return arr.filter(item => item !== valueToRemove);
 };
 
 // Do not edit below this line
