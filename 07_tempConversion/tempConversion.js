ccb3a9999e5370377fb1c00e006964bb5a8c9774
Rina-Mbugua <134137276+Rina-Mbugua@users.noreply.github.com>
2023-09-05 10:46:17 +0300

    Added solution to convertToFahrenheit function

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -4,7 +4,9 @@ const convertToCelsius = function(fahrenheit) {
 };
 
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(tempInCelsius) {
+  const tempInFahrenheit = ((tempInCelsius * (9/5))+32).toFixed(1); //we have flipped the fraction and added 32
+  return parseFloat(tempInFahrenheit);
 };
 
 // Do not edit below this line
