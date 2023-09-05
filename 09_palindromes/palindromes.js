3cd5584dc9d7e62a01d743783714ffa4ae574344
Rina-Mbugua <134137276+Rina-Mbugua@users.noreply.github.com>
2023-09-05 14:50:28 +0300

    code solution to palindrome

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,12 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    //remove spaces and convert to lower case
+    const cleanStr = str.replace(/\s/g, '').toLowerCase();
 
+    //reverse the cleaned string
+    const reversedStr = cleanStr.split('').reverse().join('');
+
+    //compare the original and reversed strings
+    return cleanStr === reversedStr
 };
 
 // Do not edit below this line
