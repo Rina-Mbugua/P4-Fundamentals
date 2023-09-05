c4cf179444d0c51f8660ad38d45e75edacafac81
Rina-Mbugua <134137276+Rina-Mbugua@users.noreply.github.com>
2023-09-05 15:28:33 +0300

    Code solution to fibonacci function

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,12 @@
-const fibonacci = function() {
+const fibonacci = function(n) {
+    const fibonacciSequence = [0, 1]; //start with the first two numbers of the sequence 
 
+    for (let i = 2; i < n ; i++) {
+        const nextNumber = fibonacciSequence[i - 1] + fibonacciSequence[i - 2];
+        fibonacciSequence.push(nextNumber);
+    }
+
+    return fibonacciSequence
 };
 
 // Do not edit below this line
