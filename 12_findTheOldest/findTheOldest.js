f1c102bb2e26d8af33c4eae244a22506f1dc0155
Rina-Mbugua <134137276+Rina-Mbugua@users.noreply.github.com>
2023-09-06 11:10:06 +0300

    code solution to findTheOldestPerson

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,19 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    let oldestPerson = null;
+    let maxAge = - Infinity;
 
+    for (const person of people) { //person is a temporary variable to hold the values of the objects one by one as the loop is iterated.
+        const birthYear = person.yearOfBirth;
+        const deathYear = person.yearOfDeath || new Date().getFullYear; //Assumes currect date if there is no yearOfDeath
+        const age = deathYear - birthYear;
+
+        if (age > maxAge) {
+            maxAge = age;
+            oldestPerson = person;
+        } // if the ages of the currect person is larger than the current age, then the new maxAge is age and the oldest person is the person/object with the oldest age 
+    }
+
+    return oldestPerson
 };
 
 // Do not edit below this line
