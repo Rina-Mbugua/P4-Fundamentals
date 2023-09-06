c6cd84fd39a6a8f579c3bd0ce8f45eaf9d725ec1
Rina-Mbugua <134137276+Rina-Mbugua@users.noreply.github.com>
2023-09-06 09:29:37 +0300

    code solution to getTheTitles function

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,9 @@
-const getTheTitles = function() {
+const getTheTitles = function(books) {
+    // Create a new array using the map function to extract book titles
+    const bookTitles = books.map(book => book.title);
 
+    // Return the array of book titles
+    return bookTitles
 };
 
 // Do not edit below this line
